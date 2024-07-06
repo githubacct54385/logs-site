2ccbb842523a04f9cf32d006b9c79e93be339b1e
Alexander Barke <alexbarke002@gmail.com>
2024-07-06 01:19:59 +0000

    fix comments

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./logs.db');
+const db = new sqlite3.Database('../holiday-tracker/holidays.db');
 
 app.set('view engine', 'ejs');
 
@@ -12,7 +12,7 @@ app.get('/', function(req, res) {
 
 app.get('/logs', (req, res) => {
   var logs = [];
-  db.all("SELECT id, createdAt, message FROM Log", (err, rows) => {
+  db.all("SELECT id, createdAt, message FROM Log order by createdAt DESC LIMIT 100", (err, rows) => {
     res.render('pages/logs', {
       logs: rows
     })
